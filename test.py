49df9187f3fb2645ba180814d4caabcefaa73007
Michael Cruz <mike@ubercruzer.com>
2020-04-13 14:26:17 -0700

    added try except for request loop

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ import time
 user_id = '42599044'
 
 
-sampFile = 'first50.json'
+sampFile = 'first100.json'
 with open(sampFile) as sample:
     sampleReq = json.load(sample)
 
@@ -23,24 +23,37 @@ totalReq = int(round(totalFollowers / 100))
 pageinationCursor = None
 
 for reqNum in range(0, totalReq):
-    # loop request
-    query = twitchIntegration.get_followers_to(user_id, first=100, after=pageinationCursor)
-    response = twitchIntegration.get_response(query)
+    try:
+        # loop request
+        query = twitchIntegration.get_followers_to(user_id, first=100, after=pageinationCursor)
+        response = twitchIntegration.get_response(query)
+        
+        # 1 second pause keep API limit happy
+        time.sleep(2)
 
-    # set pagination cursor for next loop
-    pageinationCursor = response.json()['pagination']['cursor']
+        # set pagination cursor for next loop
+        pageinationCursor = response.json()['pagination']['cursor']
 
-    # dump file 
-    fileNum = str(reqNum).zfill(5)
-    fileName = '{0}.json'.format(fileNum)
-    filePath = os.path.join(os.getcwd(), 'followers', fileName)
+        # dump file 
+        fileNum = str(reqNum).zfill(5)
+        fileName = '{0}.json'.format(fileNum)
+        filePath = os.path.join(os.getcwd(), 'followers', fileName)
 
-    with open(filePath, "w") as outfile: 
-        json.dump(response.json(), outfile) 
-    
-    # 1 second pause keep API limit happy
-    time.sleep(1)
+        with open(filePath, "w") as outfile: 
+            json.dump(response.json(), outfile) 
 
-    print('Finished', reqNum, 'out of', totalReq)
+
+        print('Finished', reqNum, 'out of', totalReq)
+    except:
+        print('Eff.')
+
+        # dump file 
+        fileName = 'busted.json'
+        filePath = os.path.join(os.getcwd(), 'followers', fileName)
+
+        with open(filePath, "w") as outfile: 
+            json.dump(response.json(), outfile) 
+        
+        break
 
 
