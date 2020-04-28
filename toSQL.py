3f7385ccb34fbbb53337dadd40a89f9c6e945802
Michael Cruz <mike@ubercruzer.com>
2020-04-28 16:09:56 -0700

    adding users to sql database

--- toSQL.py
+++ toSQL.py
@@ -1,17 +1,48 @@
 import mysql.connector
-from params import host, user, passwd
+from params import host, user, passwd, database
+import pandas as pd
+import os
+import json
+import twitchIntegration
+
+
+
+twitchAPI = twitchIntegration.twitchAPI()
 
 
 mydb = mysql.connector.connect(
     host=host,
     user=user,
     passwd=passwd,
-    auth_plugin='mysql_native_password'
+    auth_plugin='mysql_native_password',
+    database=database
 )
 
 
-# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-# val = ("John", "Highway 21")
-# mycursor.execute(sql, val)
 
-print(mydb)
+barryPath = os.path.join(os.getcwd(), 'following', 'Barrickinov0001.json')
+
+with open(barryPath) as f:
+    data = json.load(f)
+
+userList = []
+
+for i in data['data']:
+    userList.append(i['to_id'])
+
+query = twitchAPI.get_users(userList)
+response = twitchAPI.get_response(query)
+
+
+mycursor = mydb.cursor()
+
+for i in response.json()['data']:
+
+    sql = "INSERT INTO persons (user_id, display_name, view_count, broadcaster_type) VALUES (%s, %s, %s, %s)"
+    val = (i['id'], i['display_name'], i['view_count'], i['broadcaster_type'])
+    mycursor.execute(sql, val)
+
+mydb.commit()
+
+# print(mycursor.rowcount, "record inserted.")
+
